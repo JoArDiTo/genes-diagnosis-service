d36c239c663890fd04ae38edac52372d0051f883
JoArDiTo <sm.armando05@gmail.com>
2025-05-06 00:27:06 -0500

    refactor: remove UpdateObservationDto as it is no longer needed

--- src/observation/dto/update-observation.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateObservationDto } from './create-observation.dto';
-
-export class UpdateObservationDto extends PartialType(CreateObservationDto) {
-  id: number;
-}
