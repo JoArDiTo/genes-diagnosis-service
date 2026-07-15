76dcc1f5a81b30d29d34651db0e4b17e6c71b677
Paul Santillan <110622811+paulsantillan787@users.noreply.github.com>
2026-07-15 12:10:09 -0500

    Refactor to use HTTP service with CORS enabled

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { envs } from './config/envs';
 
 async function bootstrap() {
-  const logger = new Logger('DIAGNOSIS_SERVICE');
+  const logger = new Logger('DIAGNOSIS_SERVICE_HTTP');
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-      options: {
-        host: '::',
-        port: envs.port
-      }
-    }
-  );
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PATCH,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-    })
-  )
+    }),
+  );
 
-  await app.listen();
-  logger.log(`Diagnosis service running on port ${ envs.port }`);
+  const port = process.env.PORT || envs.port || 3003;
+
+  await app.listen(port, '0.0.0.0');
+
+  logger.log(`Diagnosis HTTP service running on port ${port}`);
 }
+
 bootstrap();
